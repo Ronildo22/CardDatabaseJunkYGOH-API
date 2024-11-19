10f7ec945530683d33786f81c088a21c411e8044
Ronildo22 <ronildo.santos224@gmail.com>
2024-11-19 12:36:29 -0300

    feat: add image card route

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request, send_file
+from flask_pydantic_spec import FlaskPydanticSpec
 
 
 app = Flask(__name__)
+spec = FlaskPydanticSpec(
+    backend_name='flask', 
+    title='Card Database Yu-gi-Oh Monster Archetype "Junk"', 
+    version='v1.0',
+    path="doc"
+)
+spec.register(app)
+
+
+@app.get('/cardDatabase/<card_name>')
+def get_card_image(card_name: str):
+
+    return send_file(f'../card_database/{card_name}.jpg')
 
 
 if __name__ == '__main__':
